9ae2eb760364db766f4789220ef166e2b659406f
Eric <keashaborn@users.noreply.github.com>
2026-07-18 18:49:26 +0000

    test: preserve security definer owners in review clone

--- tools/memory_v1_v5_claim_projection_review_batch_production_clone.sh
+++ tools/memory_v1_v5_claim_projection_review_batch_production_clone.sh
@@ -34,8 +34,8 @@ cleanup() {
 }
 trap cleanup EXIT
 docker exec "$container" createdb -U sage -T template0 "$clone_db"
-docker exec "$container" pg_dump -U sage -d "$source_db" -Fc --no-owner \
-  | docker exec -i "$container" pg_restore -U sage -d "$clone_db" --no-owner
+docker exec "$container" pg_dump -U sage -d "$source_db" -Fc \
+  | docker exec -i "$container" pg_restore -U sage -d "$clone_db"
 
 clone_dsn=$(SOURCE_DSN="$POSTGRES_DSN" CLONE_DB="$clone_db" python3 - <<'PY'
 import os
