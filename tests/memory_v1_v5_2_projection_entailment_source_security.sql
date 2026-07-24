6f978d6da39821df91375a97c8887ede9be08649
Eric <keashaborn@users.noreply.github.com>
2026-07-24 14:57:26 +0000

    test: separate session-bound functional probes

--- tests/memory_v1_v5_2_projection_entailment_source_security.sql
+++ tests/memory_v1_v5_2_projection_entailment_source_security.sql
@@ -35,46 +35,4 @@ BEGIN
 END
 $block$;
 
-SET LOCAL ROLE brains_app;
-SELECT set_config(
-  'app.user_id',
-  '1240822d-ac9a-4096-95aa-e2b24d36ef50',
-  true
-);
-
-DO $block$
-DECLARE
-  source record;
-BEGIN
-  SELECT * INTO STRICT source
-  FROM memory.preflight_projection_entailment_source_v5_2(
-    '560261e2-7ac6-435d-bcb6-934315b78472'::uuid
-  );
-  IF source.observation_ref<>'o00'
-     OR source.evidence_id<>
-       '22bd0732-3539-4180-8f89-8f84114131c0'::uuid
-     OR jsonb_array_length(source.source_spans)<>1 THEN
-    RAISE EXCEPTION 'owner-self V5.2 entailment source drifted';
-  END IF;
-END
-$block$;
-
-SELECT set_config(
-  'app.user_id',
-  '557ea042-cb82-48f8-9429-472e96c957ef',
-  true
-);
-
-DO $block$
-BEGIN
-  PERFORM *
-  FROM memory.preflight_projection_entailment_source_v5_2(
-    '560261e2-7ac6-435d-bcb6-934315b78472'::uuid
-  );
-  RAISE EXCEPTION 'cross-owner V5.2 entailment source unexpectedly passed';
-EXCEPTION
-  WHEN no_data_found THEN NULL;
-END
-$block$;
-
 ROLLBACK;
