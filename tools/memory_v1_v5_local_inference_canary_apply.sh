500c390d3162d99bec13ddf5e47a68fd4ba72a39
Eric <keashaborn@users.noreply.github.com>
2026-07-25 01:01:18 +0000

    memory: accept compiler-v7 persistence retry code

--- tools/memory_v1_v5_local_inference_canary_apply.sh
+++ tools/memory_v1_v5_local_inference_canary_apply.sh
@@ -284,6 +284,7 @@ if [[ "$resume_mode" == 0 && "$completed_replay_mode" == 0 ]]; then
           'local_transport_rate_limited',
           'local_transport_server_error',
           'local_validation_rejected',
+          'local_persistence_rejected',
           'local_incomplete_response',
           'local_persistence_contract_mismatch'
         )
